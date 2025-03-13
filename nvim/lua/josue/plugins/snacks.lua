80f7e4533c259f24d841936ad912721dccb2d9d6
JosueIsOffline <josuehernandez2314@gmail.com>
2025-03-12 20:35:19 -0400

    feat: add witdh to dashboard

--- nvim/lua/josue/plugins/snacks.lua
+++ nvim/lua/josue/plugins/snacks.lua
@@ -3,7 +3,10 @@ return {
   priority = 1001,
   lazy = false,
   opts = {
-    dashboard = { enabled = true },
+    dashboard = {
+      enabled = true,
+      width = 60,
+    },
     notifier = { enabled = true },
     scroll = { enabled = true },
   },
